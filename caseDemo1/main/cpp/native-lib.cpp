a31485baae87a7ddba1d121fd3b7bec2076d73a6
Romantic <m15115021148@163.com>
2021-09-02 15:17:58 +0800

    sync

--- caseDemo1/main/cpp/native-lib.cpp
+++ caseDemo1/main/cpp/native-lib.cpp
@@ -4,23 +4,23 @@
 
 extern "C" {
     JNIEXPORT void JNICALL
-    Java_xingkong_demo_nativegl_1demo_JNIProxy_CalPixel(JNIEnv *env, jclass type) {
+    Java_com_romantic_gl_OpenGLNative_init(JNIEnv *env, jclass type) {
         renderS=new GLrenderS;
         renderS->cal_pixel();
     }
 
     JNIEXPORT void JNICALL
-    Java_xingkong_demo_nativegl_1demo_JNIProxy_StartRenderS(JNIEnv *env, jclass type) {
+    Java_com_romantic_gl_OpenGLNative_startRenders(JNIEnv *env, jclass type) {
         renderS->StartRenderThread();
     }
 
     JNIEXPORT void JNICALL
-    Java_xingkong_demo_nativegl_1demo_JNIProxy_StopRenderS(JNIEnv *env, jclass type) {
+    Java_com_romantic_gl_OpenGLNative_stopRenders(JNIEnv *env, jclass type) {
         renderS->StopRenderThread();
     }
 
     JNIEXPORT void JNICALL
-    Java_xingkong_demo_nativegl_1demo_JNIProxy_SetSurfaceS(JNIEnv *env, jclass type, jobject surface) {
+    Java_com_romantic_gl_OpenGLNative_setSurfaces(JNIEnv *env, jclass type, jobject surface) {
         renderS->SetWindow(env,surface);
     }
 }
